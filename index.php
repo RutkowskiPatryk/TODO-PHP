70ee9e4bd3af32deaa7cf703610bb1981aef6a2b
Patryk Rutkowski <rutkowskipatryk1@gmail.com>
2020-05-28 22:43:59 +0200

    update index.php

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+<?php
+    include_once('connect.php');
+
+    if(isset($_GET['done'])) {
+        $statement = $dbh->prepare("DELETE FROM task WHERE id = :id");
+        $statement->bindParam('id', $_GET['done'], PDO::PARAM_INT);
+        $statement->execute();
+
+        header("Location: index.php");
+    }
+
+?>
 <!doctype html>
 <html lang="en">
   <head>
@@ -12,19 +24,18 @@
     
     <nav>
         <div class="nav_logo">
-            Todo App
+            <a href="index.php">Todo App</a>
         </div>
         <div class="nav_menu">
-            <a href="add-task.html">Add task</a>
-            <a href="#">Done</a>
+            <a href="index.php">List to do</a>
+            <a href="add-task.php">Add task</a>
         </div>
     </nav>
 
     <div class="container">
-        <div class="find">
-            <input type="text" placeholder="Search...">
+        <div class="main_header">
+            <h2>List to do</h2>
         </div>
-
         <div class="main_table">
             <table>
                 <thead>
@@ -35,40 +46,19 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>English</td>
-                        <td>English lesson online</td>
-                        <td>
-                            <button type="button" name="done">Done</button>
-                            <button type="button" name="add_task">Remove</button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>English</td>
-                        <td>English lesson online</td>
-                        <td>
-                            <button type="button" name="done">Done</button>
-                            <button type="button" name="add_task">Remove</button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>English</td>
-                        <td>English lesson online</td>
-                        <td>
-                            <button type="button" name="done">Done</button>
-                            <button type="button" name="add_task">Remove</button>
-                        </td>
-                    </tr>
+                    <?php
+                        $sql = 'SELECT id,title,description FROM task';
+                        foreach ($dbh->query($sql) as $row) {
+                            echo '<tr>';
+                            echo '<td>' . $row['title'] . '</td>';
+                            echo '<td>' . $row['description'] . '</td>';
+                            echo '<td><a href="index.php?done=' . $row['id'] . '" class="done">Done</a></td></tr>';
+                        }
+                    ?>
                 </tbody>
             </table>
         </div>
     </div>
-
-    <footer>
-        <div class="footer">
-            <p>Copyright Patryk Rutkowski</p>
-        </div>
-    </footer>
 
   </body>
 </html>
